35f2beb49e352bc095c21a32e7d6fdb6ddc78e4f
brenetic <brendan.butler@digital.justice.gov.uk>
2020-07-30 14:22:33 +0100

    Set AWS credentials in outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,14 @@ kubectl config set-credentials ${SERVICE_ACCOUNT} --token=${K8S_TOKEN}
 kubectl config set-context ${K8S_CLUSTER_NAME} --cluster=${K8S_CLUSTER_NAME} --user=${SERVICE_ACCOUNT} --namespace=${K8S_NAMESPACE}
 kubectl config use-context ${K8S_CLUSTER_NAME}
 
+AWS_ACCESS_KEY_ID=$(kubectl get secrets -n ${K8S_NAMESPACE} ${ECR_CREDENTIALS_SECRET} -o json | jq -r '.data["access_key"]' | base64 -d)
+AWS_SECRET_ACCESS_KEY=$(kubectl get secrets -n ${K8S_NAMESPACE} ${ECR_CREDENTIALS_SECRET} -o json | jq -r '.data["secret_access_key"]' | base64 -d)
+ECR_REPO_URL=$(kubectl get secrets -n ${K8S_NAMESPACE} ${ECR_CREDENTIALS_SECRET} -o json | jq -r '.data["repo_url"]' | base64 -d)
+
+echo ::set-output name=aws_access_key_id::$AWS_ACCESS_KEY_ID
+echo ::set-output name=aws_secret_access_key::$AWS_SECRET_ACCESS_KEY
+echo ::set-output name=ecr_repo_url::$ECR_REPO_URL
+
 result="$(kubectl $1)"
 
 status=$?
